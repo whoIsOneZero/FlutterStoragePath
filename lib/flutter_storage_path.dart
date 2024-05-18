920a2c358c640d7b1617bd3dd282d677c3c3f570
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-05-18 01:27:07 +0000

    bug fixes

--- lib/flutter_storage_path.dart
+++ lib/flutter_storage_path.dart
@@ -1,3 +1,4 @@
+/*
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -29,3 +30,50 @@ class StoragePath {
     return data;
   }
 }
+*/
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class StoragePath {
+  static const MethodChannel _channel = const MethodChannel('storage_path');
+
+  /// To load images path on device
+  static Future<String?> get imagesPath async {
+    await _requestPermission(Permission.photos);
+    final String? data = await _channel.invokeMethod('getImagesPath');
+    return data;
+  }
+
+  /// To load video path on device
+ /* static Future<String?> get videoPath async {
+    await _requestPermission(Permission.videos);
+    final String? data = await _channel.invokeMethod('getVideosPath');
+    return data;
+  }*/
+
+ /* /// To load audio path on device
+  static Future<String?> get audioPath async {
+    await _requestPermission(Permission.audio);
+    final String? data = await _channel.invokeMethod('getAudioPath');
+    return data;
+  }*/
+
+  /// To load file path on device
+ /* static Future<String?> get filePath async {
+    await _requestPermission(Permission.manageExternalStorage);
+    final String? data = await _channel.invokeMethod('getFilesPath');
+    return data;
+  }*/
+
+  static Future<void> _requestPermission(Permission permission) async {
+    if (await permission.isGranted) {
+      return;
+    }
+
+    await permission.request();
+  }
+}
+
